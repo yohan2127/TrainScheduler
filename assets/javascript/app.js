65c45b37132e8359e5c1fa7fac8befb482363e1d
yohan2127 <zedamek@yahoo.com>
2019-02-21 18:12:50 -0500

    update for delete script

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,23 +6,23 @@ var config = {
   storageBucket: "trainscheduler-1a67a.appspot.com",
   messagingSenderId: "309691806229"
 };
+var name ='';
+var destination = '';
+var firstTrainTime = '';
+var frequency = '';
+var nextTrain = '';
+var nextTrainFormatted = '';
+var minutesAway = '';
+var firstTimeConverted = '';
+var currentTime = '';
+var diffTime = '';
+var tRemainder = '';
+var minutesTillTrain = '';
+var keyHolder = '';
+var getKey = '';
+
 firebase.initializeApp(config);
-
 var database = firebase.database();
-
-// Initial Values
-var trainName = "";
-var destination = "";
-var firstTrainTime = "";
-var frequency = 0;
-var currentTime = moment();
-var index = 0;
-var trainIDs = [];
-
-// Show current time
-var datetime = null,
-date = null;
-
 var update = function () {
   date = moment(new Date())
   datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
@@ -33,88 +33,62 @@ $(document).ready(function(){
   update();
   setInterval(update, 1000);
 });
+$(document).ready(function() {
 
+     $("#add-train").on("click", function() {
+     	
+     	name = $('#train-name').val().trim();
+     	destination = $('#destination').val().trim();
+     	firstTrainTime = $('#train-time').val().trim();
+     	frequency = $('#frequency').val().trim();
+          firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+          currentTime = moment();
+          diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+          tRemainder = diffTime % frequency;
+          minutesTillTrain = frequency - tRemainder;
+          nextTrain = moment().add(minutesTillTrain, "minutes");
+          nextTrainFormatted = moment(nextTrain).format("hh:mm");
 
-// Capture Button Click
-$("#add-train").on("click", function() {
+     	// Code for the push
+     	keyHolder = database.ref().push({
+     		name: name,
+     		destination: destination,
+     		firstTrainTime: firstTrainTime,  // 2:22 in my example
+     		frequency: frequency,
+            nextTrainFormatted: nextTrainFormatted,
+            minutesTillTrain: minutesTillTrain
+     	});alert("added schedule succesfully!!!");
+        $('#train-name').val('');
+     	$('#destination').val('');
+     	$('#train-time').val('');
+     	$('#frequency').val('');
 
-  // Grabbed values from text boxes
-  trainName = $("#train-name").val().trim();
-  destination = $("#destination").val().trim();
-  firstTrainTime = $("#train-time").val().trim();
-  frequency = $("#frequency").val().trim();
-  var firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
-  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-  var tRemainder = diffTime % childSnapshot.val().frequency;
-  var minutesAway = frequency - tRemainder;
-  var nextTrain = moment().add(minutesAway, "minutes");
-  var nextArrival = moment(nextTrain).format("hh:mm a");
-
-  var nextArrivalUpdate = function() {
-    date = moment(new Date())
-    datetime.html(date.format('hh:mm a'));
-	
-  }
-
-  // Code for handling the push
-  database.ref().push({
-    trainName: trainName,
-    destination: destination,
-    firstTrainTime: firstTrainTime,
-    frequency: frequency,
-    minutesAway: minutesAway,
-    nextArrival: nextArrival,
-    dateAdded: firebase.database.ServerValue.TIMESTAMP
-  });
+     	return false;
+     });
   
-  alert("Schedule submitted!");
+     database.ref().on("child_added", function(childSnapshot) {
+		$('#new-train').append("<tr class='table-row' id=" + "'" + childSnapshot.key + "'" + ">" +
+               "<td class='col-xs-3'>" + childSnapshot.val().name +
+               "</td>" +
+               "<td class='col-xs-2'>" + childSnapshot.val().destination +
+               "</td>" +
+               "<td class='col-xs-2'>" + childSnapshot.val().frequency +
+               "</td>" +
+               "<td class='col-xs-2'>" + childSnapshot.val().nextTrainFormatted + 
+               "</td>" +
+               "<td class='col-xs-2'>" + childSnapshot.val().minutesTillTrain + 
+               "</td>" +
+               "<td class='col-xs-1'>" + "<input type='submit' value='remove train' class='remove-train btn btn-primary btn-sm'>" + "</td>" +
+          "</tr>");
 
-  // Empty text input
-  $("#train-name").val("");
-  $("#destination").val("");
-  $("#train-time").val("");
-  $("#frequency").val("");
-  
-  // Don't refresh the page!
-  return false; 
+}, function(errorObject){
+
 });
-  database.ref().orderByChild("dateAdded").limitToLast(25).on("child_added", function(snapshot) {
 
+$("body").on("click", ".remove-train", function(){
+     $(this).closest ('tr').remove();
+     getKey = $(this).parent().parent().attr('id');
+     database.ref().child(getKey).remove();
+});
 
-    console.log("Train name: " + snapshot.val().trainName);
-    console.log("Destination: " + snapshot.val().destination);
-    console.log("First train: " + snapshot.val().firstTrainTime);
-    console.log("Frequency: " + snapshot.val().frequency);
-    console.log("Next train: " + snapshot.val().nextArrival);
-    console.log("Minutes away: " + snapshot.val().minutesAway);
-    console.log("==============================");
-
-
-  // Change the HTML to reflect
-  $("#new-train").append("<tr><td>" + snapshot.val().trainName + "</td>" +
-    "<td>" + snapshot.val().destination + "</td>" + 
-    "<td>" + "Every " + snapshot.val().frequency + " mins" + "</td>" + 
-    "<td>" + snapshot.val().nextArrival + "</td>" +
-    "<td>" + snapshot.val().minutesAway + " mins until arrival" + "</td>" +
-    "</td></tr>");
-
-  index++;
-
-  // Handle the errors
-  }, function(errorObject) {
-    console.log("Errors handled: " + errorObject.code);
-  });
-
-  //Gets the train IDs in an Array
-  database.ref().once('value', function(dataSnapshot){ 
-    var trainIndex = 0;
-
-      dataSnapshot.forEach(
-          function(childSnapshot) {
-              trainIDs[trainIndex++] = childSnapshot.key();
-          }
-      );
-  });
-
-  console.log(trainIDs);
-
+}); 
